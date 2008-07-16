733f88fad458288dc49cf388641b23efe206a2d4
Olli Salli <olli.salli@collabora.co.uk>
2008-07-16 12:42:11 +0300

    Add license headers to qt4-{constants,types}-gen

--- tools/qt4-constants-gen.py
+++ tools/qt4-constants-gen.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python
+#
+# Copyright (C) 2008 Collabora Limited <http://www.collabora.co.uk>
+# Copyright (C) 2008 Nokia Corporation
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 from sys import argv, stdout, stderr
 import xml.dom.minidom
