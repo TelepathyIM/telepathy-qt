75359f01cacd844c4ddb0274147608cf1a78642c
Olli Salli <olli.salli@collabora.co.uk>
2008-09-22 19:19:47 +0300

    We depend on automake >= 1.9, reflect that in the ./autogen.sh automake chooser check

--- autogen.sh
+++ autogen.sh
@@ -3,13 +3,13 @@ set -e
 
 if test -n "$AUTOMAKE"; then
     : # don't override an explicit user request
-elif automake-1.8 --version >/dev/null 2>/dev/null && \
-     aclocal-1.8 --version >/dev/null 2>/dev/null; then
-    # If we have automake-1.8, use it. This helps to ensure that our build
-    # system doesn't accidentally grow automake-1.9 dependencies.
-    AUTOMAKE=automake-1.8
+elif automake-1.9 --version >/dev/null 2>/dev/null && \
+     aclocal-1.9 --version >/dev/null 2>/dev/null; then
+    # If we have automake-1.9, use it. This helps to ensure that our build
+    # system doesn't accidentally grow automake-1.10 dependencies.
+    AUTOMAKE=automake-1.9
     export AUTOMAKE
-    ACLOCAL=aclocal-1.8
+    ACLOCAL=aclocal-1.9
     export ACLOCAL
 fi
 
