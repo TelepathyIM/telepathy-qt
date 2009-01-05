86b5870182dc43a540de73a268376a5999a54402
Andre Moreira Magalhaes (andrunko) <andre.magalhaes@collabora.co.uk>
2009-01-05 18:58:39 -0300

    Improved manager-file test case.

--- tests/manager-file.cpp
+++ tests/manager-file.cpp
@@ -27,8 +27,10 @@ void TestManagerFile::testManagerFile()
     ManagerFile managerFile("test-manager-file");
     QCOMPARE(managerFile.isValid(), true);
 
-    QCOMPARE(managerFile.protocols(),
-             QStringList() << "foo" << "bar");
+    QStringList protocols = managerFile.protocols();
+    protocols.sort();
+    QCOMPARE(protocols,
+             QStringList() << "bar" << "foo" << "somewhat-pathological");
 
     ParamSpecList params = managerFile.parameters("foo");
     QCOMPARE(containsParam(params, "account"), true);
@@ -49,6 +51,43 @@ void TestManagerFile::testManagerFile()
     param = getParam(params, "encryption-key");
     QCOMPARE(param->flags, (uint) ConnMgrParamFlagSecret);
     QCOMPARE(param->signature, QString("s"));
+
+    params = managerFile.parameters("somewhat-pathological");
+    QCOMPARE(containsParam(params, "foo"), true);
+    QCOMPARE(containsParam(params, "semicolons"), true);
+    QCOMPARE(containsParam(params, "list"), true);
+    QCOMPARE(containsParam(params, "unterminated-list"), true);
+    QCOMPARE(containsParam(params, "spaces-in-list"), true);
+    QCOMPARE(containsParam(params, "escaped-semicolon-in-list"), true);
+    QCOMPARE(containsParam(params, "doubly-escaped-semicolon-in-list"), true);
+    QCOMPARE(containsParam(params, "triply-escaped-semicolon-in-list"), true);
+    QCOMPARE(containsParam(params, "empty-list"), true);
+    QCOMPARE(containsParam(params, "escaped-semicolon"), true);
+    QCOMPARE(containsParam(params, "object"), true);
+    QCOMPARE(containsParam(params, "non-existant"), false);
+
+    param = getParam(params, "foo");
+    QCOMPARE(param->flags, (uint) ConnMgrParamFlagRequired | ConnMgrParamFlagHasDefault);
+    QCOMPARE(param->signature, QString("s"));
+    param = getParam(params, "semicolons");
+    QCOMPARE(param->flags, (uint) ConnMgrParamFlagSecret | ConnMgrParamFlagHasDefault);
+    QCOMPARE(param->signature, QString("s"));
+    param = getParam(params, "list");
+    QCOMPARE(param->signature, QString("as"));
+    QCOMPARE(param->defaultValue.variant().toStringList(),
+             QStringList() << "list" << "of" << "misc");
+    param = getParam(params, "escaped-semicolon-in-list");
+    QCOMPARE(param->signature, QString("as"));
+    QCOMPARE(param->defaultValue.variant().toStringList(),
+             QStringList() << "list;of" << "misc");
+    param = getParam(params, "doubly-escaped-semicolon-in-list");
+    QCOMPARE(param->signature, QString("as"));
+    QCOMPARE(param->defaultValue.variant().toStringList(),
+             QStringList() << "list\\" << "of" << "misc");
+    param = getParam(params, "triply-escaped-semicolon-in-list");
+    QCOMPARE(param->signature, QString("as"));
+    QCOMPARE(param->defaultValue.variant().toStringList(),
+             QStringList() << "list\\;of" << "misc");
 }
 
 bool containsParam(const ParamSpecList &params, const char *name)
