a515a3682e269b10011518925f6d41ff09ec1494
George Goldberg <george.goldberg@collabora.co.uk>
2009-01-29 21:52:24 +0000

    Add a PendingReadyAccount which is a subclass of PendingOperation that will be returned by Account::becomReady() allowing to get the Account easily from the slot triggered when the operation completes.

---
+++ TelepathyQt4/Client/pending-ready-account.cpp
@@ -0,0 +1,110 @@
+/*
+ * This file is part of TelepathyQt4
+ *
+ * Copyright (C) 2009 Collabora Ltd. <http://www.collabora.co.uk/>
+ * Copyright (C) 2009 Nokia Corporation
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <TelepathyQt4/Client/PendingReadyAccount>
+
+#include "TelepathyQt4/Client/_gen/pending-ready-account.moc.hpp"
+
+#include "TelepathyQt4/debug-internal.h"
+
+#include <QDBusPendingCallWatcher>
+#include <QDBusPendingReply>
+
+/**
+ * \addtogroup clientsideproxies Client-side proxies
+ *
+ * Proxy objects representing remote service objects accessed via D-Bus.
+ *
+ * In addition to providing direct access to methods, signals and properties
+ * exported by the remote objects, some of these proxies offer features like
+ * automatic inspection of remote object capabilities, property tracking,
+ * backwards compatibility helpers for older services and other utilities.
+ */
+
+namespace Telepathy
+{
+namespace Client
+{
+
+struct PendingReadyAccount::Private
+{
+    Private(Account::Features features, Account *account) :
+        features(features),
+        account(account)
+    {
+    }
+
+    Account::Features features;
+    Account *account;
+};
+
+/**
+ * \class PendingReadyAccount
+ * \ingroup clientaccount
+ * \headerfile <TelepathyQt4/Client/pending-ready-account.h> <TelepathyQt4/Client/PendingReadyAccount>
+ *
+ * Class containing the features requested and the reply to a request
+ * for an account to become ready. Instances of this class cannot be
+ * constructed directly; the only way to get one is via Account::becomeReady().
+ */
+
+/**
+ * Construct a PendingReadyAccount object.
+ *
+ * \param account The Account that will become ready.
+ */
+PendingReadyAccount::PendingReadyAccount(Account::Features features, Account *account)
+    : PendingOperation(account),
+      mPriv(new Private(features, account))
+{
+}
+
+/**
+ * Class destructor.
+ */
+PendingReadyAccount::~PendingReadyAccount()
+{
+    delete mPriv;
+}
+
+/**
+ * Return the Account object through which the request was made.
+ *
+ * \return Account object.
+ */
+Account *PendingReadyAccount::account() const
+{
+    return mPriv->account;
+}
+
+/**
+ * Return the Features that were requested to become ready on the
+ * account.
+ *
+ * \return Features.
+ */
+Account::Features PendingReadyAccount::features() const
+{
+    return mPriv->features;
+}
+
+} // Telepathy::Client
+} // Telepathy
---
+++ TelepathyQt4/Client/pending-ready-account.h
@@ -0,0 +1,62 @@
+/*
+ * This file is part of TelepathyQt4
+ *
+ * Copyright (C) 2009 Collabora Ltd. <http://www.collabora.co.uk/>
+ * Copyright (C) 2009 Nokia Corporation
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef _TelepathyQt4_cli_pending_ready_account_h_HEADER_GUARD_
+#define _TelepathyQt4_cli_pending_ready_account_h_HEADER_GUARD_
+
+#ifndef IN_TELEPATHY_QT4_HEADER
+#error IN_TELEPATHY_QT4_HEADER
+#endif
+
+#include <TelepathyQt4/Client/Account>
+#include <TelepathyQt4/Client/PendingOperation>
+
+class QDBusPendingCallWatcher;
+
+namespace Telepathy
+{
+namespace Client
+{
+
+class PendingReadyAccount : public PendingOperation
+{
+    Q_OBJECT
+
+public:
+    ~PendingReadyAccount();
+
+    Account *account() const;
+    Account::Features features() const;
+
+private:
+    Q_DISABLE_COPY(PendingReadyAccount);
+    PendingReadyAccount(Account::Features features, Account *account);
+
+    struct Private;
+    friend struct Private;
+    friend class Account;
+    Private *mPriv;
+};
+
+} // Telepathy::Client
+} // Telepathy
+
+#endif
