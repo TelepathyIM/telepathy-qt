6703724947b4501ca0a82f719346aa14c95374cf
Andre Moreira Magalhaes (andrunko) <andre.magalhaes@collabora.co.uk>
2009-04-14 03:22:28 -0300

    autogen.sh: Added some command line args that simulate autotools configure script.
    Added support --enable-debug/disable-debug/enable-compiler-coverage/prefix.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,37 @@
 #!/bin/sh
 
+flags=
+debug=
+profile=
+
+for i in $*; do
+    case $i in
+    --enable-debug)
+        debug="yes"
+        ;;
+    --disable-debug)
+        debug="no"
+        ;;
+    --enable-compiler-coverage)
+        profile=true
+        ;;
+    --prefix=*)
+        prefix=`echo $i | sed 's/[-a-zA-Z0-9]*=//'`
+        flags="$flags -DCMAKE_BUILD_PREFIX=$prefix"
+        ;;
+    esac
+done
+
 mkdir -p build
 cd build
-cmake ..
+
+if [ "$profile" ]; then
+    flags="$flags -DCMAKE_BUILD_TYPE=Profile"
+elif [ "$debug" == "yes" ]; then
+    flags="$flags -DCMAKE_BUILD_TYPE=Debug"
+elif [ "$debug" == "no" ]; then
+    flags="$flags -DCMAKE_BUILD_TYPE=Release"
+fi
+
+echo "running cmake $flags .."
+cmake $flags ..
