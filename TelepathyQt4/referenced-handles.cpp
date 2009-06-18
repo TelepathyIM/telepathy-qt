be215fa7428da5c620e5b31e71f5761ec3ec2a11
Andre Moreira Magalhaes (andrunko) <andre.magalhaes@collabora.co.uk>
2009-06-18 12:26:22 -0300

    ReferencedHandles: Moved docs to source file.

--- TelepathyQt4/referenced-handles.cpp
+++ TelepathyQt4/referenced-handles.cpp
@@ -28,6 +28,25 @@
 #include <QPointer>
 #include <QSharedData>
 
+/**
+ * \addtogroup clientsideproxies Client-side proxies
+ *
+ * Proxy objects representing remote service objects accessed via D-Bus.
+ *
+ * In addition to providing direct access to methods, signals and properties
+ * exported by the remote objects, some of these proxies offer features like
+ * automatic inspection of remote object capabilities, property tracking,
+ * backwards compatibility helpers for older services and other utilities.
+ */
+
+/**
+ * \defgroup clientconn Connection proxies
+ * \ingroup clientsideproxies
+ *
+ * Proxy objects representing remote Telepathy Connections and their optional
+ * interfaces.
+ */
+
 namespace Tp
 {
 
@@ -93,6 +112,25 @@ struct ReferencedHandles::Private : public QSharedData
 private:
     void operator=(const Private&);
 };
+
+/**
+ * \class ReferencedHandles
+ * \ingroup clientconn
+ * \headerfile <TelepathyQt4/referenced-handles.h> <TelepathyQt4/ReferencedHandles>
+ *
+ * Helper container for safe management of handle lifetimes. Every handle in a
+ * ReferencedHandles container is guaranteed to be valid (and stay valid, as
+ * long it's in at least one ReferencedHandles container).
+ *
+ * The class offers a QList-style API. However, from the mutable operations,
+ * only the operations for which the validity guarantees can be preserved are
+ * provided. This means no functions which can add an arbitrary handle to the
+ * container are included - the only way to add handles to the container is to
+ * reference them using Connection::referenceHandles() and appending the
+ * resulting ReferenceHandles instance.
+ *
+ * ReferencedHandles is a implicitly shared class.
+ */
 
 ReferencedHandles::ReferencedHandles()
     : mPriv(new Private)
