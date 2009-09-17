12c0009bfee93784bb66aba3884d4a8fb248f5a7
Andre Moreira Magalhaes (andrunko) <andre.magalhaes@collabora.co.uk>
2009-09-17 17:06:50 -0300

    FileTransferChannelCreationProperties: Added class to be used by Account::createFileTransfer.

---
+++ TelepathyQt4/file-transfer-channel-creation-properties.cpp
@@ -0,0 +1,168 @@
+/*
+ * This file is part of TelepathyQt4
+ *
+ * Copyright (C) 2009 Collabora Ltd. <http://www.collabora.co.uk/>
+ * Copyright (C) 2009 Nokia Corporation
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <TelepathyQt4/FileTransferChannelCreationProperties>
+
+#include <QSharedData>
+
+namespace Tp
+{
+
+struct FileTransferChannelCreationProperties::Private : public QSharedData
+{
+    Private(const QString &suggestedFileName, const QString &contentType,
+            qulonglong size)
+        : suggestedFileName(suggestedFileName),
+          contentType(contentType),
+          size(size),
+          hasContentHash(false), contentHashType(FileHashTypeNone),
+          hasDescription(false),
+          hasLastModificationTime(false)
+    {
+    }
+
+    /* mandatory parameters */
+    QString suggestedFileName;
+    QString contentType;
+    qulonglong size;
+
+    /* optional parameters */
+    bool hasContentHash;
+    FileHashType contentHashType;
+    QString contentHash;
+
+    bool hasDescription;
+    QString description;
+
+    bool hasLastModificationTime;
+    QDateTime lastModificationTime;
+};
+
+FileTransferChannelCreationProperties::FileTransferChannelCreationProperties(
+        const QString &suggestedFileName, const QString &contentType,
+        qulonglong size)
+    : mPriv(new Private(suggestedFileName, contentType, size))
+{
+}
+
+FileTransferChannelCreationProperties::FileTransferChannelCreationProperties(
+        const FileTransferChannelCreationProperties &other)
+    : mPriv(other.mPriv)
+{
+}
+
+
+FileTransferChannelCreationProperties::~FileTransferChannelCreationProperties()
+{
+}
+
+FileTransferChannelCreationProperties &FileTransferChannelCreationProperties::operator=(
+        const FileTransferChannelCreationProperties &other)
+{
+    if (this != &other) {
+        mPriv = other.mPriv;
+    }
+    return *this;
+}
+
+bool FileTransferChannelCreationProperties::operator==(
+        const FileTransferChannelCreationProperties &other) const
+{
+    return mPriv == other.mPriv;
+}
+
+FileTransferChannelCreationProperties &FileTransferChannelCreationProperties::setContentHash(
+        FileHashType contentHashType, const QString &contentHash)
+{
+    mPriv->hasContentHash = (contentHashType != FileHashTypeNone);
+    mPriv->contentHashType = contentHashType;
+    mPriv->contentHash = contentHash;
+    return *this;
+}
+
+FileTransferChannelCreationProperties &FileTransferChannelCreationProperties::setDescription(
+        const QString &description)
+{
+    mPriv->hasDescription = (!description.isEmpty());
+    mPriv->description = description;
+    return *this;
+}
+
+FileTransferChannelCreationProperties &FileTransferChannelCreationProperties::setLastModificationTime(
+        const QDateTime &lastModificationTime)
+{
+    mPriv->hasLastModificationTime = (lastModificationTime.isValid());
+    mPriv->lastModificationTime = lastModificationTime;
+    return *this;
+}
+
+QString FileTransferChannelCreationProperties::suggestedFileName() const
+{
+    return mPriv->suggestedFileName;
+}
+
+QString FileTransferChannelCreationProperties::contentType() const
+{
+    return mPriv->contentType;
+}
+
+qulonglong FileTransferChannelCreationProperties::size() const
+{
+    return mPriv->size;
+}
+
+bool FileTransferChannelCreationProperties::hasContentHash() const
+{
+    return mPriv->hasContentHash;
+
+}
+
+FileHashType FileTransferChannelCreationProperties::contentHashType() const
+{
+    return mPriv->contentHashType;
+}
+
+QString FileTransferChannelCreationProperties::contentHash() const
+{
+    return mPriv->contentHash;
+}
+
+bool FileTransferChannelCreationProperties::hasDescription() const
+{
+    return mPriv->hasDescription;
+}
+
+QString FileTransferChannelCreationProperties::description() const
+{
+    return mPriv->description;
+}
+
+bool FileTransferChannelCreationProperties::hasLastModificationTime() const
+{
+    return mPriv->hasLastModificationTime;
+}
+
+QDateTime FileTransferChannelCreationProperties::lastModificationTime() const
+{
+    return mPriv->lastModificationTime;
+}
+
+} // Tp
---
+++ TelepathyQt4/file-transfer-channel-creation-properties.h
@@ -0,0 +1,90 @@
+/*
+ * This file is part of TelepathyQt4
+ *
+ * Copyright (C) 2009 Collabora Ltd. <http://www.collabora.co.uk/>
+ * Copyright (C) 2009 Nokia Corporation
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef _TelepathyQt4_file_transfer_channel_creation_properties_h_HEADER_GUARD_
+#define _TelepathyQt4_file_transfer_channel_creation_properties_h_HEADER_GUARD_
+
+#ifndef IN_TELEPATHY_QT4_HEADER
+#error IN_TELEPATHY_QT4_HEADER
+#endif
+
+#include <TelepathyQt4/Constants>
+
+#include <QDateTime>
+#include <QSharedDataPointer>
+#include <QString>
+
+namespace Tp
+{
+
+class FileTransferChannelCreationProperties
+{
+public:
+    FileTransferChannelCreationProperties(const QString &suggestedFileName,
+            const QString &contentType, qulonglong size);
+    FileTransferChannelCreationProperties(
+            const FileTransferChannelCreationProperties &other);
+    ~FileTransferChannelCreationProperties();
+
+    FileTransferChannelCreationProperties &operator=(
+            const FileTransferChannelCreationProperties &other);
+    bool operator==(
+            const FileTransferChannelCreationProperties &other) const;
+    inline bool operator!=(
+            const FileTransferChannelCreationProperties &other) const
+    {
+        return !(*this == other);
+    }
+
+    FileTransferChannelCreationProperties &setContentHash(
+            FileHashType contentHashType, const QString &contentHash);
+    FileTransferChannelCreationProperties &setDescription(
+            const QString &description);
+    FileTransferChannelCreationProperties &setLastModificationTime(
+            const QDateTime &lastModificationTime);
+
+private:
+    friend class Account;
+
+    /* mandatory parameters */
+    QString suggestedFileName() const;
+    QString contentType() const;
+    qulonglong size() const;
+
+    /* optional parameters */
+    bool hasContentHash() const;
+    FileHashType contentHashType() const;
+    QString contentHash() const;
+
+    bool hasDescription() const;
+    QString description() const;
+
+    bool hasLastModificationTime() const;
+    QDateTime lastModificationTime() const;
+
+    struct Private;
+    friend struct Private;
+    QSharedDataPointer<Private> mPriv;
+};
+
+} // Tp
+
+#endif
