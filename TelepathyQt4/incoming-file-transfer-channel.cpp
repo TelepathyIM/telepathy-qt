abbdc32ce83da11c0b2469f66dfbc94be130180f
Andre Moreira Magalhaes (andrunko) <andre.magalhaes@collabora.co.uk>
2009-09-23 10:06:08 -0300

    Revert "IncomingFileTransferChannel: Added Port access control support."
    This reverts commit 7e4a6e7db7dc4ac86370e5f8af7d18ad53d0d9da.
    
    Conflicts:
    
    	TelepathyQt4/incoming-file-transfer-channel.cpp

--- TelepathyQt4/incoming-file-transfer-channel.cpp
+++ TelepathyQt4/incoming-file-transfer-channel.cpp
@@ -31,7 +31,6 @@
 #include <TelepathyQt4/Types>
 
 #include <QIODevice>
-#include <QTcpServer>
 #include <QTcpSocket>
 
 namespace Tp
@@ -47,7 +46,6 @@ struct IncomingFileTransferChannel::Private
 
     QIODevice *output;
     QTcpSocket *socket;
-    SocketAccessControl accessControl;
     SocketAddressIPv4 addr;
 
     qulonglong requestedOffset;
@@ -171,37 +169,11 @@ PendingOperation *IncomingFileTransferChannel::acceptFile(qulonglong offset,
 
     mPriv->output = output;
 
-    QDBusVariant accessControlParam;
-    SupportedSocketMap socketTypes = availableSocketTypes();
-    UIntList addressTypeList = socketTypes.value(SocketAddressTypeIPv4);
-    if (addressTypeList.contains(SocketAccessControlPort)) {
-        debug() << "Using SocketAccessControlPort";
-
-        QTcpServer tcpServer;
-        tcpServer.listen(QHostAddress("127.0.0.1"));
-        quint16 port = tcpServer.serverPort();
-        tcpServer.close();
-
-        SocketAddressIPv4 addr;
-        addr.address = "127.0.0.1";
-        addr.port = port;
-
-        mPriv->accessControl = SocketAccessControlPort;
-        // set port here
-        mPriv->addr.port = port;
-        accessControlParam.setVariant(qVariantFromValue(addr));
-    } else {
-        debug() << "Using SocketAccessControlLocalhost";
-
-        mPriv->accessControl = SocketAccessControlLocalhost;
-        accessControlParam = QDBusVariant(QVariant(QString()));
-    }
-
     mPriv->requestedOffset = offset;
 
     PendingVariant *pv = new PendingVariant(
             fileTransferInterface(BypassInterfaceCheck)->AcceptFile(SocketAddressTypeIPv4,
-                mPriv->accessControl, accessControlParam,
+                SocketAccessControlLocalhost, QDBusVariant(QVariant(QString())),
                 offset),
             this);
     connect(pv,
@@ -219,13 +191,10 @@ void IncomingFileTransferChannel::onAcceptFileFinished(PendingOperation *op)
         return;
     }
 
-    if (mPriv->accessControl == SocketAccessControlLocalhost) {
-        PendingVariant *pv = qobject_cast<PendingVariant *>(op);
-        mPriv->addr = qdbus_cast<SocketAddressIPv4>(pv->result());
-    } else {
-        mPriv->addr.address = "127.0.0.1";
-        // the port was already set in acceptFile
-    }
+    PendingVariant *pv = qobject_cast<PendingVariant *>(op);
+    mPriv->addr = qdbus_cast<SocketAddressIPv4>(pv->result());
+    debug().nospace() << "Got address " << mPriv->addr.address <<
+        ":" << mPriv->addr.port;
 
     if (state() == FileTransferStateOpen) {
         // now we have the address and we are already opened,
