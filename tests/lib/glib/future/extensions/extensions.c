6c33cb6a63f2a5115c28f6c1e1e94efee966b06c
Andre Moreira Magalhaes (andrunko) <andre.magalhaes@collabora.co.uk>
2010-02-26 03:26:43 -0300

    Added glib tests future extensions library.
    The library is intended to be used by regression tests wanting to test new
    features not included in telepathy-glib upstream.
    
    Patch from Simon McVittie (smcv) <simon.mcvittie@collabora.co.uk>

---
+++ tests/lib/glib/future/extensions/extensions.c
@@ -0,0 +1,6 @@
+#include "extensions.h"
+
+/* include auto-generated stubs for things common to service and client */
+#include "_gen/gtypes-body.h"
+#include "_gen/interfaces-body.h"
+#include "_gen/signals-marshal.h"
---
+++ tests/lib/glib/future/extensions/extensions.h
@@ -0,0 +1,18 @@
+#ifndef FUTURE_EXTENSIONS_H
+#define FUTURE_EXTENSIONS_H
+
+#include <glib-object.h>
+#include <telepathy-glib/channel.h>
+
+#include "tests/lib/glib/future/extensions/_gen/enums.h"
+#include "tests/lib/glib/future/extensions/_gen/svc-channel.h"
+#include "tests/lib/glib/future/extensions/_gen/svc-misc.h"
+
+G_BEGIN_DECLS
+
+#include "tests/lib/glib/future/extensions/_gen/gtypes.h"
+#include "tests/lib/glib/future/extensions/_gen/interfaces.h"
+
+G_END_DECLS
+
+#endif
