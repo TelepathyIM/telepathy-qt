49e48891fb1d17dae6f41f2023e617abe9fd405e
Andre Moreira Magalhaes (andrunko) <andre.magalhaes@collabora.co.uk>
2010-06-29 21:57:33 -0300

    Added valgrind support when running tests.

---
+++ tools/valgrind.mk
@@ -0,0 +1,12 @@
+VALGRIND = valgrind --tool=memcheck \
+    --verbose \
+    --leak-check=full \
+    --leak-resolution=high \
+    --child-silent-after-fork=yes \
+    --num-callers=20 \
+    --gen-suppressions=all
+
+# other potentially interesting options:
+# --show-reachable=yes   reachable objects (many!)
+# --read-var-info=yes    better diagnostics from DWARF3 info
+# --track-origins=yes    better diagnostics for uninit values (slow)
