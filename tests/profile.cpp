7cf9a347060cc611dc48d382ec58f96d2ed6a172
Andre Moreira Magalhaes (andrunko) <andre.magalhaes@collabora.co.uk>
2010-08-19 04:01:37 -0300

    profile test: Added test for parameters.

--- tests/profile.cpp
+++ tests/profile.cpp
@@ -22,6 +22,29 @@ void TestProfile::testProfile()
     QCOMPARE(profile->name(), QLatin1String("TestProfile"));
     QCOMPARE(profile->cmName(), QLatin1String("testprofilecm"));
     QCOMPARE(profile->protocolName(), QLatin1String("testprofileproto"));
+
+    QCOMPARE(profile->parameters().isEmpty(), false);
+    QCOMPARE(profile->parameters().count(), 2);
+
+    QCOMPARE(profile->hasParameter(QLatin1String("foo")), false);
+
+    QCOMPARE(profile->hasParameter(QLatin1String("server")), true);
+    Profile::Parameter param = profile->parameter(QLatin1String("server"));
+    QCOMPARE(param.name(), QLatin1String("server"));
+    QCOMPARE(param.dbusSignature(), QDBusSignature(QLatin1String("s")));
+    QCOMPARE(param.type(), QVariant::String);
+    QCOMPARE(param.value(), QVariant(QLatin1String("profile.com")));
+    QCOMPARE(param.label(), QString());
+    QCOMPARE(param.isMandatory(), true);
+
+    QCOMPARE(profile->hasParameter(QLatin1String("port")), true);
+    param = profile->parameter(QLatin1String("port"));
+    QCOMPARE(param.name(), QLatin1String("port"));
+    QCOMPARE(param.dbusSignature(), QDBusSignature(QLatin1String("u")));
+    QCOMPARE(param.type(), QVariant::UInt);
+    QCOMPARE(param.value(), QVariant(QLatin1String("1111")));
+    QCOMPARE(param.label(), QString());
+    QCOMPARE(param.isMandatory(), true);
 }
 
 QTEST_MAIN(TestProfile)
