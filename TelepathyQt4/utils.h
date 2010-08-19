3765fe1fbc140d6fcb91945911b6c63a38b64579
Andre Moreira Magalhaes (andrunko) <andre.magalhaes@collabora.co.uk>
2010-08-19 02:28:24 -0300

    Added utils.h containing some utility functions that will be used by Profiles.
    The methods are copied from ManagerFile/KeyFile with slightly modifications.

--- TelepathyQt4/utils.h
+++ TelepathyQt4/utils.h
@@ -19,20 +19,34 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#ifndef _TelepathyQt4_debug_h_HEADER_GUARD_
-#define _TelepathyQt4_debug_h_HEADER_GUARD_
+#ifndef _TelepathyQt4_utils_h_HEADER_GUARD_
+#define _TelepathyQt4_utils_h_HEADER_GUARD_
 
 #ifndef IN_TELEPATHY_QT4_HEADER
 #error IN_TELEPATHY_QT4_HEADER
 #endif
 
 #include <TelepathyQt4/Global>
+
+#include <QByteArray>
+#include <QDBusSignature>
 #include <QString>
+#include <QStringList>
+#include <QVariant>
 
 namespace Tp
 {
 
 TELEPATHY_QT4_EXPORT QString escapeAsIdentifier(const QString &string);
+
+TELEPATHY_QT4_EXPORT bool unescapeString(const QByteArray &data, int from, int to,
+        QString &result);
+TELEPATHY_QT4_EXPORT bool unescapeStringList(const QByteArray &data, int from, int to,
+        QStringList &result);
+
+TELEPATHY_QT4_EXPORT QVariant::Type variantTypeFromDBusSignature(const QDBusSignature &signature);
+TELEPATHY_QT4_EXPORT QVariant variantFromValueWithDBusSignature(const QString &value,
+        const QDBusSignature &signature);
 
 } // Tp
 
