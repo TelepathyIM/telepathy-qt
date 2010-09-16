4377ca88b15bf60a34c2079563824dbe46af31f4
Dario Freddi <dario.freddi@collabora.co.uk>
2010-09-16 22:37:38 +0200

    cmake: Make make dist behave like autotools' make dist, and port all the tools target to CMake as well

--- cmake/modules/TelepathyDist.cmake
+++ cmake/modules/TelepathyDist.cmake
@@ -1,7 +1,33 @@
 # setup make dist
-add_custom_target(dist cd ${CMAKE_SOURCE_DIR} &&
-                        git archive --format=tar --prefix=${PACKAGE_NAME}-${PACKAGE_VERSION}/ HEAD |
-                            gzip > ${CMAKE_BINARY_DIR}/${PACKAGE_NAME}-${PACKAGE_VERSION}.tar.gz)
+add_custom_command(OUTPUT ${CMAKE_BINARY_DIR}/${PACKAGE_NAME}-${PACKAGE_VERSION}.tar.gz
+                   COMMAND git archive --format=tar --prefix=${PACKAGE_NAME}-${PACKAGE_VERSION}/ HEAD |
+                           gzip > ${CMAKE_BINARY_DIR}/${PACKAGE_NAME}-${PACKAGE_VERSION}.tar.gz
+                   WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
+
+add_custom_target(create-source-working-dir
+                  rm -rf ${PACKAGE_NAME}-${PACKAGE_VERSION} &&
+                  gzip -df ${PACKAGE_NAME}-${PACKAGE_VERSION}.tar.gz &&
+                  tar -xf ${PACKAGE_NAME}-${PACKAGE_VERSION}.tar &&
+                  rm ${PACKAGE_NAME}-${PACKAGE_VERSION}.tar* &&
+                  cd ${PACKAGE_NAME}-${PACKAGE_VERSION}/ &&
+                  mkdir doc && cp -R ${CMAKE_BINARY_DIR}/doc/html doc/
+
+                  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+                  DEPENDS ${CMAKE_BINARY_DIR}/${PACKAGE_NAME}-${PACKAGE_VERSION}.tar.gz)
+add_dependencies(create-source-working-dir doxygen-doc)
+
+add_custom_target(dist-hook
+                  chmod u+w ${CMAKE_BINARY_DIR}/${PACKAGE_NAME}-${PACKAGE_VERSION}/ChangeLog &&
+                  git log --stat > ${CMAKE_BINARY_DIR}/${PACKAGE_NAME}-${PACKAGE_VERSION}/ChangeLog ||
+                  git log > ${CMAKE_BINARY_DIR}/${PACKAGE_NAME}-${PACKAGE_VERSION}/ChangeLog
+
+                  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
+add_dependencies(dist-hook create-source-working-dir)
+
+add_custom_target(dist tar --format=ustar -chf - ${PACKAGE_NAME}-${PACKAGE_VERSION} |
+                       GZIP=--best gzip -c > ${PACKAGE_NAME}-${PACKAGE_VERSION}.tar.gz
+                  WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+add_dependencies(dist dist-hook)
 
 # setup make distcheck
 add_custom_target(distcheck cd ${CMAKE_BINARY_DIR} &&
