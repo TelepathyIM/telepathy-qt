f1c9ee1c36b8813b32b06509b1e84a9208459f09
Andre Moreira Magalhaes (andrunko) <andre.magalhaes@collabora.co.uk>
2011-03-18 16:21:08 -0300

    contact-messenger example: Add simple example to send messages using ContactMessenger.

---
+++ examples/contact-messenger/sender.cpp
@@ -0,0 +1,81 @@
+/**
+ * This file is part of TelepathyQt4
+ *
+ * @copyright Copyright (C) 2011 Collabora Ltd. <http://www.collabora.co.uk/>
+ * @copyright Copyright (C) 2011 Nokia Corporation
+ * @license LGPL 2.1
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "sender.h"
+#include "_gen/sender.moc.hpp"
+
+#include <TelepathyQt4/Account>
+#include <TelepathyQt4/Debug>
+#include <TelepathyQt4/Constants>
+#include <TelepathyQt4/ContactMessenger>
+#include <TelepathyQt4/PendingSendMessage>
+#include <TelepathyQt4/Types>
+
+#include <QCoreApplication>
+
+Sender::Sender(const QString &accountPath,
+        const QString &contactIdentifier, const QString &message)
+{
+    Tp::AccountPtr acc = Tp::Account::create(TP_QT4_IFACE_ACCOUNT, accountPath);
+    messenger = Tp::ContactMessenger::create(acc, contactIdentifier);
+    connect(messenger->sendMessage(message),
+            SIGNAL(finished(Tp::PendingOperation*)),
+            SLOT(onSendMessageFinished(Tp::PendingOperation*)));
+}
+
+Sender::~Sender()
+{
+}
+
+void Sender::onSendMessageFinished(Tp::PendingOperation *op)
+{
+    if (op->isError()) {
+        qDebug() << "Error sending message:" << op->errorName() << "-" << op->errorMessage();
+        QCoreApplication::exit(1);
+        return;
+    }
+
+    Tp::PendingSendMessage *psm = qobject_cast<Tp::PendingSendMessage *>(op);
+    qDebug() << "Message sent, token is" << psm->sentMessageToken();
+    QCoreApplication::exit(0);
+}
+
+int main(int argc, char **argv)
+{
+    QCoreApplication app(argc, argv);
+
+    Tp::registerTypes();
+    Tp::enableDebug(true);
+    Tp::enableWarnings(true);
+
+    if (argc < 4) {
+        qDebug() << "Usage: contact-messenger account_path contact_id message";
+        return -1;
+    }
+
+    Sender *sender = new Sender(QLatin1String(argv[1]), QLatin1String(argv[2]),
+            QLatin1String(argv[3]));
+
+    int ret = app.exec();
+    delete sender;
+    return ret;
+}
---
+++ examples/contact-messenger/sender.h
@@ -0,0 +1,51 @@
+/**
+ * This file is part of TelepathyQt4
+ *
+ * @copyright Copyright (C) 2011 Collabora Ltd. <http://www.collabora.co.uk/>
+ * @copyright Copyright (C) 2011 Nokia Corporation
+ * @license LGPL 2.1
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef _TelepathyQt4_examples_contact_messenger_sender_h_HEADER_GUARD_
+#define _TelepathyQt4_examples_contact_messenger_sender_h_HEADER_GUARD_
+
+#include <TelepathyQt4/Types>
+
+#include <QObject>
+#include <QString>
+
+namespace Tp
+{
+class PendingOperation;
+}
+
+class Sender : public QObject
+{
+    Q_OBJECT
+
+public:
+    Sender(const QString &accountPath, const QString &contactIdentifier, const QString &message);
+    ~Sender();
+
+private Q_SLOTS:
+    void onSendMessageFinished(Tp::PendingOperation *op);
+
+private:
+    Tp::ContactMessengerPtr messenger;
+};
+
+#endif
