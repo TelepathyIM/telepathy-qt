8fa2bb6e9c953669ccc2dfe7ec9402be52871880
Olli Salli <ollisal@gmail.com>
2011-04-10 20:09:31 +0300

    Test conn status change during introspection

--- tests/dbus/conn-introspect-cornercases.cpp
+++ tests/dbus/conn-introspect-cornercases.cpp
@@ -19,6 +19,7 @@
 #include <telepathy-glib/debug.h>
 
 #include <tests/lib/glib/bug16307-conn.h>
+#include <tests/lib/glib/simple-conn.h>
 #include <tests/lib/test.h>
 
 using namespace Tp;
@@ -39,6 +40,7 @@ private Q_SLOTS:
     void initTestCase();
     void init();
 
+    void testStatusChange();
     void testSlowpath();
 
     void cleanup();
@@ -120,6 +122,87 @@ void TestConnIntrospectCornercases::testSlowpath()
     QCOMPARE(mLoop->exec(), 0);
     QCOMPARE(op->isFinished(), true);
     QCOMPARE(mConn->isReady(Connection::FeatureCore), true);
+    QCOMPARE(static_cast<uint>(mConn->status()),
+             static_cast<uint>(ConnectionStatusConnected));
+}
+
+void TestConnIntrospectCornercases::testStatusChange()
+{
+    gchar *name;
+    gchar *connPath;
+    GError *error = 0;
+
+    TpTestsSimpleConnection *simpleConnService =
+        TP_TESTS_SIMPLE_CONNECTION(
+            g_object_new(
+                TP_TESTS_TYPE_SIMPLE_CONNECTION,
+                "account", "me@example.com",
+                "protocol", "simple",
+                NULL));
+    QVERIFY(simpleConnService != 0);
+
+    mConnService = TP_BASE_CONNECTION(simpleConnService);
+    QVERIFY(mConnService != 0);
+
+    QVERIFY(tp_base_connection_register(mConnService, "simple",
+                &name, &connPath, &error));
+    QVERIFY(error == 0);
+
+    mConn = Connection::create(QLatin1String(name), QLatin1String(connPath),
+            ChannelFactory::create(QDBusConnection::sessionBus()),
+            ContactFactory::create());
+    QCOMPARE(mConn->isReady(), false);
+
+    g_free(name); name = 0;
+    g_free(connPath); connPath = 0;
+
+    // Make core ready first, because Connection has internal handling for the status changing
+    // during core introspection, and we rather want to test the more general ReadinessHelper
+    // mechanism
+
+    PendingOperation *op = mConn->becomeReady();
+    QVERIFY(connect(op,
+                    SIGNAL(finished(Tp::PendingOperation*)),
+                    SLOT(expectSuccessfulCall(Tp::PendingOperation*))));
+
+    QCOMPARE(mLoop->exec(), 0);
+    QCOMPARE(op->isFinished(), true);
+    QCOMPARE(mConn->isReady(Connection::FeatureCore), true);
+    QCOMPARE(static_cast<uint>(mConn->status()),
+             static_cast<uint>(ConnectionStatusDisconnected));
+
+    // Now, begin making Connected ready
+
+    op = mConn->becomeReady(Connection::FeatureConnected);
+    QVERIFY(connect(op,
+                    SIGNAL(finished(Tp::PendingOperation*)),
+                    SLOT(expectSuccessfulCall(Tp::PendingOperation*))));
+
+    mLoop->processEvents();
+
+    // But disturb it by changing the status!
+
+    TpHandleRepoIface *contact_repo = tp_base_connection_get_handles(mConnService,
+            TP_HANDLE_TYPE_CONTACT);
+
+    mConnService->self_handle = tp_handle_ensure(contact_repo, "me@example.com",
+            NULL, NULL);
+
+    tp_base_connection_change_status(mConnService,
+            TP_CONNECTION_STATUS_CONNECTING,
+            TP_CONNECTION_STATUS_REASON_REQUESTED);
+
+    // Do that again! (The earlier op stil hasn't finished by definition)
+    mConn->becomeReady(Features() << Connection::FeatureConnected);
+
+    tp_base_connection_change_status(mConnService,
+            TP_CONNECTION_STATUS_CONNECTED,
+            TP_CONNECTION_STATUS_REASON_REQUESTED);
+
+    QCOMPARE(mLoop->exec(), 0);
+    QCOMPARE(op->isFinished(), true);
+    QCOMPARE(mConn->isReady(Connection::FeatureCore), true);
+    QCOMPARE(mConn->isReady(Connection::FeatureConnected), true);
     QCOMPARE(static_cast<uint>(mConn->status()),
              static_cast<uint>(ConnectionStatusConnected));
 }
