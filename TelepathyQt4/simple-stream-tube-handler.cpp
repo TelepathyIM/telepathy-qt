4f8391c8eed8c06c0cab99dc69636442341e2de7
Olli Salli <ollisal@gmail.com>
2011-05-03 21:06:42 +0300

    Add SimpleStreamTubeHandler implementation helper
    Internal for now. Could perhaps be generalized to a generic "simple handler"... but it's a slightly
    risky public class, as the user has to make sure they somehow handle all emitted channels and don't
    ignore some.

---
+++ TelepathyQt4/simple-stream-tube-handler.cpp
@@ -0,0 +1,241 @@
+/**
+ * This file is part of TelepathyQt4
+ *
+ * @copyright Copyright (C) 2011 Collabora Ltd. <http://www.collabora.co.uk/>
+ * @copyright Copyright (C) 2011 Nokia Corporation
+ * @license LGPL 2.1
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "TelepathyQt4/simple-stream-tube-handler.h"
+
+#include "TelepathyQt4/_gen/simple-stream-tube-handler.moc.hpp"
+
+#include "TelepathyQt4/debug-internal.h"
+
+#include <TelepathyQt4/Account>
+#include <TelepathyQt4/ChannelClassSpec>
+#include <TelepathyQt4/PendingComposite>
+#include <TelepathyQt4/PendingReady>
+#include <TelepathyQt4/StreamTubeChannel>
+
+namespace Tp
+{
+
+namespace
+{
+    ChannelClassSpecList buildFilter(const QStringList &services, bool requested)
+    {
+        Q_ASSERT(!services.isEmpty());
+
+        ChannelClassSpecList filter;
+
+        foreach (QString service, services)
+        {
+            filter.append(requested ?
+                    ChannelClassSpec::outgoingStreamTube(service) :
+                    ChannelClassSpec::incomingStreamTube(service));
+        }
+
+        return filter;
+    }
+}
+
+SharedPtr<SimpleStreamTubeHandler> SimpleStreamTubeHandler::create(
+        const QStringList &services,
+        bool requested,
+        bool monitorConnections)
+{
+    return SharedPtr<SimpleStreamTubeHandler>(
+            new SimpleStreamTubeHandler(
+                services,
+                requested,
+                monitorConnections));
+}
+
+SimpleStreamTubeHandler::SimpleStreamTubeHandler(
+        const QStringList &services,
+        bool requested,
+        bool monitorConnections)
+: AbstractClientHandler(buildFilter(services, requested)),
+    mMonitorConnections(monitorConnections)
+{
+}
+
+SimpleStreamTubeHandler::~SimpleStreamTubeHandler()
+{
+    if (!mTubes.empty()) {
+        debug() << "~SSTubeHandler(): Closing" << mTubes.size() << "leftover tubes";
+
+        foreach (StreamTubeChannelPtr tube, mTubes.keys()) {
+            tube->requestClose();
+        }
+    }
+}
+
+void SimpleStreamTubeHandler::handleChannels(
+        const MethodInvocationContextPtr<> &context,
+        const AccountPtr &account,
+        const ConnectionPtr &connection,
+        const QList<ChannelPtr> &channels,
+        const QList<ChannelRequestPtr> &requestsSatisfied,
+        const QDateTime &userActionTime,
+        const HandlerInfo &handlerInfo)
+{
+    debug() << "SimpleStreamTubeHandler::handleChannels() invoked for " <<
+        channels.size() << "channels on account" << account->objectPath();
+
+    SharedPtr<InvocationData> invocation(new InvocationData());
+    QList<PendingOperation *> readyOps;
+
+    foreach (ChannelPtr chan, channels) {
+        StreamTubeChannelPtr tube = StreamTubeChannelPtr::qObjectCast(chan);
+
+        if (!tube) {
+            if (chan->channelType() != TP_QT4_IFACE_CHANNEL_TYPE_STREAM_TUBE) {
+                warning() << "We got a non-StreamTube channel" << chan->objectPath();
+                context->setFinishedWithError(TP_QT4_ERROR_INCONSISTENT,
+                        QLatin1String(
+                            "Invoked with non-StreamTube channels, which don't match our filter"));
+                return;
+            } else {
+                warning() << "The channel factory used for a simple StreamTube handler must" <<
+                    "construct StreamTubeChannel subclasses for stream tubes";
+                context->setFinishedWithError(TP_QT4_ERROR_SERVICE_CONFUSED,
+                        QLatin1String(
+                            "ChannelFactory specifies invalid channel subclass for stream tubes"));
+                return;
+            }
+        }
+
+        Features features = StreamTubeChannel::FeatureStreamTube;
+        if (mMonitorConnections) {
+            features.insert(StreamTubeChannel::FeatureConnectionMonitoring);
+        }
+        readyOps.append(tube->becomeReady(features));
+
+        invocation->tubes.append(tube);
+    }
+
+    invocation->ctx = context;
+    invocation->acc = account;
+    invocation->time = userActionTime;
+
+    if (!requestsSatisfied.isEmpty()) {
+        invocation->hints = requestsSatisfied.first()->hints();
+    }
+
+    invocation->readyOp = new PendingComposite(readyOps, SharedPtr<SimpleStreamTubeHandler>(this));
+    connect(invocation->readyOp,
+            SIGNAL(finished(Tp::PendingOperation*)),
+            SLOT(onReadyOpFinished(Tp::PendingOperation*)));
+
+    mInvocations.append(invocation);
+}
+
+QList<QPair<AccountPtr, StreamTubeChannelPtr> > SimpleStreamTubeHandler::tubes() const
+{
+    QList<QPair<AccountPtr, StreamTubeChannelPtr> > tubes;
+
+    foreach (StreamTubeChannelPtr tube, mTubes.keys()) {
+        tubes.append(qMakePair(mTubes.value(tube), tube));
+    }
+
+    return tubes;
+}
+
+void SimpleStreamTubeHandler::onReadyOpFinished(Tp::PendingOperation *op)
+{
+    Q_ASSERT(!mInvocations.isEmpty());
+    Q_ASSERT(op->isFinished());
+
+    for (QLinkedList<SharedPtr<InvocationData> >::iterator i = mInvocations.begin();
+            i != mInvocations.end(); ++i) {
+        if ((*i)->readyOp != op) {
+            continue;
+        }
+
+        (*i)->readyOp = 0;
+
+        if (op->isError()) {
+            warning() << "Preparing proxies for SSTubeHandler failed with" << op->errorName()
+                << op->errorMessage();
+            (*i)->error = op->errorName();
+            (*i)->message = op->errorMessage();
+        }
+
+        break;
+    }
+
+    while (!mInvocations.isEmpty() && !mInvocations.first()->readyOp) {
+        SharedPtr<InvocationData> invocation = mInvocations.takeFirst();
+
+        if (!invocation->error.isEmpty()) {
+            // We guarantee that the proxies were ready - so we can't invoke the client if they
+            // weren't made ready successfully. Fix the introspection code if this happens :)
+            invocation->ctx->setFinishedWithError(invocation->error, invocation->message);
+            continue;
+        }
+
+        debug() << "Emitting SSTubeHandler::invokedForTube for" << invocation->tubes.size()
+            << "tubes";
+
+        foreach (StreamTubeChannelPtr tube, invocation->tubes) {
+            if (!tube->isValid()) {
+                debug() << "Skipping already invalidated tube" << tube->objectPath();
+                continue;
+            }
+
+            if (!mTubes.contains(tube)) {
+                connect(tube.data(),
+                        SIGNAL(invalidated(Tp::DBusProxy*,QString,QString)),
+                        SLOT(onTubeInvalidated(Tp::DBusProxy*,QString,QString)));
+
+                mTubes.insert(tube, invocation->acc);
+            }
+
+            emit invokedForTube(
+                    invocation->acc,
+                    tube,
+                    invocation->time,
+                    invocation->hints);
+        }
+
+        invocation->ctx->setFinished();
+    }
+}
+
+void SimpleStreamTubeHandler::onTubeInvalidated(DBusProxy *proxy,
+        const QString &errorName, const QString &errorMessage)
+{
+    StreamTubeChannelPtr tube(qobject_cast<StreamTubeChannel *>(proxy));
+
+    Q_ASSERT(!tube.isNull());
+    Q_ASSERT(mTubes.contains(tube));
+
+    debug() << "Tube" << tube->objectPath() << "invalidated - " << errorName << ':' << errorMessage;
+
+    AccountPtr acc = mTubes.value(tube);
+    mTubes.remove(tube);
+
+    emit tubeInvalidated(
+            acc,
+            tube,
+            errorName,
+            errorMessage);
+}
+
+} // Tp
---
+++ TelepathyQt4/simple-stream-tube-handler.h
@@ -0,0 +1,112 @@
+/**
+ * This file is part of TelepathyQt4
+ *
+ * @copyright Copyright (C) 2011 Collabora Ltd. <http://www.collabora.co.uk/>
+ * @copyright Copyright (C) 2011 Nokia Corporation
+ * @license LGPL 2.1
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <TelepathyQt4/AbstractClientHandler>
+#include <TelepathyQt4/ChannelRequestHints>
+#include <TelepathyQt4/RefCounted>
+#include <TelepathyQt4/Types>
+
+#include <QDateTime>
+#include <QLinkedList>
+#include <QHash>
+#include <QQueue>
+#include <QSet>
+
+namespace Tp
+{
+
+class PendingOperation;
+
+class TELEPATHY_QT4_NO_EXPORT SimpleStreamTubeHandler : public QObject,
+                public AbstractClientHandler
+{
+    Q_OBJECT
+    Q_DISABLE_COPY(SimpleStreamTubeHandler)
+
+public:
+    static SharedPtr<SimpleStreamTubeHandler> create(
+            const QStringList &services,
+            bool requested,
+            bool monitorConnections);
+    ~SimpleStreamTubeHandler();
+
+    bool monitorsConnections() const
+    {
+        return mMonitorConnections;
+    }
+
+    bool bypassApproval() const
+    {
+        return false;
+    }
+
+    void handleChannels(const MethodInvocationContextPtr<> &context,
+            const AccountPtr &account,
+            const ConnectionPtr &connection,
+            const QList<ChannelPtr> &channels,
+            const QList<ChannelRequestPtr> &requestsSatisfied,
+            const QDateTime &userActionTime,
+            const HandlerInfo &handlerInfo);
+
+    QList<QPair<AccountPtr, StreamTubeChannelPtr> > tubes() const;
+
+Q_SIGNALS:
+    void invokedForTube(
+            const Tp::AccountPtr &account,
+            const Tp::StreamTubeChannelPtr &tube,
+            const QDateTime &userActionTime,
+            const Tp::ChannelRequestHints &requestHints);
+    void tubeInvalidated(
+            const Tp::AccountPtr &account,
+            const Tp::StreamTubeChannelPtr &tube,
+            const QString &errorName,
+            const QString &errorMessage);
+
+private Q_SLOTS:
+    void onReadyOpFinished(Tp::PendingOperation *);
+    void onTubeInvalidated(Tp::DBusProxy *, const QString &, const QString &);
+
+private:
+    SimpleStreamTubeHandler(
+            const QStringList &services,
+            bool requested,
+            bool monitorConnections);
+
+    bool mMonitorConnections;
+
+    struct InvocationData : RefCounted {
+        InvocationData() : readyOp(0) {}
+
+        PendingOperation *readyOp;
+        QString error, message;
+
+        MethodInvocationContextPtr<> ctx;
+        AccountPtr acc;
+        QList<StreamTubeChannelPtr> tubes;
+        QDateTime time;
+        ChannelRequestHints hints;
+    };
+    QLinkedList<SharedPtr<InvocationData> > mInvocations;
+    QHash<StreamTubeChannelPtr, AccountPtr> mTubes;
+};
+
+} // Tp
