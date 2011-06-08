37cf06e64ade0a93f0339aeb9a91c268634ad52c
Andre Moreira Magalhaes (andrunko) <andre.magalhaes@collabora.co.uk>
2011-06-07 22:50:44 -0300

    contact-factory test: Use TestConnHelper.

--- tests/dbus/contact-factory.cpp
+++ tests/dbus/contact-factory.cpp
@@ -1,22 +1,16 @@
-#include <QtCore/QDebug>
-#include <QtTest/QtTest>
+#include <tests/lib/test.h>
 
-#define TP_QT4_ENABLE_LOWLEVEL_API
+#include <tests/lib/glib-helpers/test-conn-helper.h>
+
+#include <tests/lib/glib/contacts-conn.h>
 
 #include <TelepathyQt4/ChannelFactory>
 #include <TelepathyQt4/Connection>
-#include <TelepathyQt4/ConnectionLowlevel>
 #include <TelepathyQt4/ContactFactory>
-#include <TelepathyQt4/PendingReady>
-#include <TelepathyQt4/Types>
 
 #include <telepathy-glib/debug.h>
 
-#include <tests/lib/glib/contacts-conn.h>
-#include <tests/lib/test.h>
-
 using namespace Tp;
-using namespace Tp::Client;
 
 class TestContactFactory : public Test
 {
@@ -24,7 +18,7 @@ class TestContactFactory : public Test
 
 public:
     TestContactFactory(QObject *parent = 0)
-        : Test(parent), mConnService(0)
+        : Test(parent), mConn(0)
     { }
 
 private Q_SLOTS:
@@ -37,9 +31,7 @@ private Q_SLOTS:
     void cleanupTestCase();
 
 private:
-    TpTestsContactsConnection *mConnService;
-    QString mConnName, mConnPath;
-    ConnectionPtr mConn;
+    TestConnHelper *mConn;
 };
 
 void TestContactFactory::initTestCase()
@@ -51,28 +43,14 @@ void TestContactFactory::initTestCase()
     tp_debug_set_flags("all");
     dbus_g_bus_get(DBUS_BUS_STARTER, 0);
 
-    gchar *name;
-    gchar *connPath;
-    GError *error = 0;
-
-    mConnService = TP_TESTS_CONTACTS_CONNECTION(g_object_new(
+    mConn = new TestConnHelper(this,
+            ChannelFactory::create(QDBusConnection::sessionBus()),
+            ContactFactory::create(Contact::FeatureAlias),
             TP_TESTS_TYPE_CONTACTS_CONNECTION,
-            "account", "me1@example.com",
+            "account", "me@example.com",
             "protocol", "simple",
-            NULL));
-    QVERIFY(mConnService != 0);
-    QVERIFY(tp_base_connection_register(TP_BASE_CONNECTION(mConnService),
-                "contacts", &name, &connPath, &error));
-    QVERIFY(error == 0);
-
-    QVERIFY(name != 0);
-    QVERIFY(connPath != 0);
-
-    mConnName = QLatin1String(name);
-    mConnPath = QLatin1String(connPath);
-
-    g_free(name);
-    g_free(connPath);
+            NULL);
+    QCOMPARE(mConn->isReady(), false);
 }
 
 void TestContactFactory::init()
@@ -82,20 +60,12 @@ void TestContactFactory::init()
 
 void TestContactFactory::testConnectionSelfContactFeatures()
 {
-    mConn = Connection::create(mConnName, mConnPath,
-            ChannelFactory::create(QDBusConnection::sessionBus()),
-            ContactFactory::create(Contact::FeatureAlias));
-    QCOMPARE(mConn->contactFactory()->features().size(), 1);
-    QVERIFY(mConn->contactFactory()->features().contains(Contact::FeatureAlias));
+    QCOMPARE(mConn->client()->contactFactory()->features().size(), 1);
+    QVERIFY(mConn->client()->contactFactory()->features().contains(Contact::FeatureAlias));
 
-    QVERIFY(connect(mConn->lowlevel()->requestConnect(Connection::FeatureSelfContact),
-                SIGNAL(finished(Tp::PendingOperation*)),
-                SLOT(expectSuccessfulCall(Tp::PendingOperation*))));
-    QCOMPARE(mLoop->exec(), 0);
-    QCOMPARE(mConn->isReady(Connection::FeatureSelfContact), true);
-    QCOMPARE(mConn->status(), ConnectionStatusConnected);
+    QCOMPARE(mConn->connect(Connection::FeatureSelfContact), true);
 
-    ContactPtr selfContact = mConn->selfContact();
+    ContactPtr selfContact = mConn->client()->selfContact();
     QVERIFY(!selfContact.isNull());
     QVERIFY(selfContact->requestedFeatures().contains(Contact::FeatureAlias));
 }
@@ -107,25 +77,8 @@ void TestContactFactory::cleanup()
 
 void TestContactFactory::cleanupTestCase()
 {
-    if (mConn) {
-        // Disconnect and wait for the readiness change
-        QVERIFY(connect(mConn->lowlevel()->requestDisconnect(),
-                        SIGNAL(finished(Tp::PendingOperation*)),
-                        SLOT(expectSuccessfulCall(Tp::PendingOperation*))));
-        QCOMPARE(mLoop->exec(), 0);
-
-        if (mConn->isValid()) {
-            QVERIFY(connect(mConn.data(),
-                            SIGNAL(invalidated(Tp::DBusProxy *, const QString &, const QString &)),
-                            SLOT(expectConnInvalidated())));
-            QCOMPARE(mLoop->exec(), 0);
-        }
-    }
-
-    if (mConnService != 0) {
-        g_object_unref(mConnService);
-        mConnService = 0;
-    }
+    QCOMPARE(mConn->disconnect(), true);
+    delete mConn;
 
     cleanupTestCaseImpl();
 }
