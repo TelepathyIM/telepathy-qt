3c8f98541d690571681444074b62de3d981e3a07
Andre Moreira Magalhaes (andrunko) <andre.magalhaes@collabora.co.uk>
2011-06-07 23:04:45 -0300

    chan-conference test: Properly set test name.

--- tests/dbus/chan-conference.cpp
+++ tests/dbus/chan-conference.cpp
@@ -76,7 +76,7 @@ void TestConferenceChan::initTestCase()
     initTestCaseImpl();
 
     g_type_init();
-    g_set_prgname("conference-chan");
+    g_set_prgname("chan-conference");
     tp_debug_set_flags("all");
     dbus_g_bus_get(DBUS_BUS_STARTER, 0);
 
