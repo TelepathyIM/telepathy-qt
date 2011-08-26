27b6b3a0ac152e7ed6ee7fb781f0d3ca6d625fd5
Olli Salli <ollisal@gmail.com>
2011-08-26 15:37:35 +0300

    Test basic StreamTubeServer export/handle operation
    SSTHandler coverage 70%, STS 43%

--- tests/dbus/stream-tube-handlers.cpp
+++ tests/dbus/stream-tube-handlers.cpp
@@ -5,10 +5,16 @@
 #include <tests/lib/glib/simple-conn.h>
 #include <tests/lib/glib/stream-tube-chan.h>
 
+#include <TelepathyQt4/Account>
+#include <TelepathyQt4/AccountManager>
+#include <TelepathyQt4/ChannelClassSpec>
 #include <TelepathyQt4/ClientHandlerInterface>
+#include <TelepathyQt4/ClientRegistrar>
 #include <TelepathyQt4/Connection>
 #include <TelepathyQt4/IncomingStreamTubeChannel>
 #include <TelepathyQt4/OutgoingStreamTubeChannel>
+#include <TelepathyQt4/PendingAccount>
+#include <TelepathyQt4/PendingReady>
 #include <TelepathyQt4/ReferencedHandles>
 #include <TelepathyQt4/StreamTubeChannel>
 #include <TelepathyQt4/StreamTubeClient>
@@ -26,24 +32,217 @@ using namespace Tp::Client;
 namespace
 {
 
+class ChannelRequestAdaptor : public QDBusAbstractAdaptor
+{
+    Q_OBJECT
+    Q_CLASSINFO("D-Bus Interface", "org.freedesktop.Telepathy.ChannelRequest")
+    Q_CLASSINFO("D-Bus Introspection", ""
+"  <interface name=\"org.freedesktop.Telepathy.ChannelRequest\" >\n"
+"    <property name=\"Account\" type=\"o\" access=\"read\" />\n"
+"    <property name=\"UserActionTime\" type=\"x\" access=\"read\" />\n"
+"    <property name=\"PreferredHandler\" type=\"s\" access=\"read\" />\n"
+"    <property name=\"Requests\" type=\"aa{sv}\" access=\"read\" />\n"
+"    <property name=\"Interfaces\" type=\"as\" access=\"read\" />\n"
+"    <property name=\"Hints\" type=\"a{sv}\" access=\"read\" />\n"
+"    <method name=\"Proceed\" />\n"
+"    <method name=\"Cancel\" />\n"
+"    <signal name=\"Failed\" >\n"
+"      <arg name=\"Error\" type=\"s\" />\n"
+"      <arg name=\"Message\" type=\"s\" />\n"
+"    </signal>\n"
+"    <signal name=\"Succeeded\" />\n"
+"    <signal name=\"SucceededWithChannel\" >\n"
+"      <arg name=\"Connection\" type=\"o\" />\n"
+"      <arg name=\"ConnectionProperties\" type=\"a{sv}\" />\n"
+"      <arg name=\"Channel\" type=\"o\" />\n"
+"      <arg name=\"ChannelProperties\" type=\"a{sv}\" />\n"
+"    </signal>\n"
+"  </interface>\n"
+        "")
+
+    Q_PROPERTY(QDBusObjectPath Account READ Account)
+    Q_PROPERTY(qulonglong UserActionTime READ UserActionTime)
+    Q_PROPERTY(QString PreferredHandler READ PreferredHandler)
+    Q_PROPERTY(QualifiedPropertyValueMapList Requests READ Requests)
+    Q_PROPERTY(QStringList Interfaces READ Interfaces)
+    Q_PROPERTY(QVariantMap Hints READ Hints)
+
+public:
+    ChannelRequestAdaptor(QDBusObjectPath account,
+            qulonglong userActionTime,
+            QString preferredHandler,
+            QualifiedPropertyValueMapList requests,
+            QStringList interfaces,
+            bool shouldFail,
+            bool proceedNoop,
+            QVariantMap hints,
+            QObject *parent)
+        : QDBusAbstractAdaptor(parent),
+          mAccount(account), mUserActionTime(userActionTime),
+          mPreferredHandler(preferredHandler), mRequests(requests),
+          mInterfaces(interfaces), mShouldFail(shouldFail),
+          mProceedNoop(proceedNoop), mHints(hints)
+    {
+    }
+
+    virtual ~ChannelRequestAdaptor()
+    {
+    }
+
+    void setChan(const QString &connPath, const QVariantMap &connProps,
+            const QString &chanPath, const QVariantMap &chanProps)
+    {
+        mConnPath = connPath;
+        mConnProps = connProps;
+        mChanPath = chanPath;
+        mChanProps = chanProps;
+    }
+
+public: // Properties
+    inline QDBusObjectPath Account() const
+    {
+        return mAccount;
+    }
+
+    inline qulonglong UserActionTime() const
+    {
+        return mUserActionTime;
+    }
+
+    inline QString PreferredHandler() const
+    {
+        return mPreferredHandler;
+    }
+
+    inline QualifiedPropertyValueMapList Requests() const
+    {
+        return mRequests;
+    }
+
+    inline QStringList Interfaces() const
+    {
+        return mInterfaces;
+    }
+
+    inline QVariantMap Hints() const
+    {
+        return mHints;
+    }
+
+public Q_SLOTS: // Methods
+    void Proceed()
+    {
+        if (mProceedNoop) {
+            return;
+        }
+
+        if (mShouldFail) {
+            QTimer::singleShot(0, this, SLOT(fail()));
+        } else {
+            QTimer::singleShot(0, this, SLOT(succeed()));
+        }
+    }
+
+    void Cancel()
+    {
+        Q_EMIT Failed(QLatin1String(TELEPATHY_ERROR_CANCELLED), QLatin1String("Cancelled"));
+    }
+
+Q_SIGNALS: // Signals
+    void Failed(const QString &error, const QString &message);
+    void Succeeded();
+    void SucceededWithChannel(const QDBusObjectPath &connPath, const QVariantMap &connProps,
+            const QDBusObjectPath &chanPath, const QVariantMap &chanProps);
+
+private Q_SLOTS:
+    void fail()
+    {
+        Q_EMIT Failed(QLatin1String(TELEPATHY_ERROR_NOT_AVAILABLE), QLatin1String("Not available"));
+    }
+
+    void succeed()
+    {
+        if (!mConnPath.isEmpty() && !mChanPath.isEmpty()) {
+            Q_EMIT SucceededWithChannel(QDBusObjectPath(mConnPath), mConnProps,
+                    QDBusObjectPath(mChanPath), mChanProps);
+        }
+
+        Q_EMIT Succeeded();
+    }
+
+private:
+    QDBusObjectPath mAccount;
+    qulonglong mUserActionTime;
+    QString mPreferredHandler;
+    QualifiedPropertyValueMapList mRequests;
+    QStringList mInterfaces;
+    bool mShouldFail;
+    bool mProceedNoop;
+    QVariantMap mHints;
+    QString mConnPath, mChanPath;
+    QVariantMap mConnProps, mChanProps;
+};
+
 void destroySocketControlList(gpointer data)
 {
-    g_array_free((GArray *) data, TRUE);
+    g_array_free(reinterpret_cast<GArray *>(data), TRUE);
 }
 
-// TODO: turn into something which supports everything, or everything except port/credentials ACs
-GHashTable *createSupportedSocketTypesHash(TpSocketAddressType addressType,
-        TpSocketAccessControl accessControl)
+GHashTable *createSupportedSocketTypesHash(bool supportMonitoring)
 {
     GHashTable *ret;
     GArray *tab;
+    TpSocketAccessControl ac;
 
     ret = g_hash_table_new_full(NULL, NULL, NULL, destroySocketControlList);
 
+    // IPv4
     tab = g_array_sized_new(FALSE, FALSE, sizeof(TpSocketAccessControl), 1);
-    g_array_append_val(tab, accessControl);
+    ac = TP_SOCKET_ACCESS_CONTROL_LOCALHOST;
+    g_array_append_val(tab, ac);
 
-    g_hash_table_insert(ret, GUINT_TO_POINTER(addressType), tab);
+    if (supportMonitoring) {
+        ac = TP_SOCKET_ACCESS_CONTROL_PORT;
+        g_array_append_val(tab, ac);
+    }
+
+    g_hash_table_insert(ret, GUINT_TO_POINTER(TP_SOCKET_ADDRESS_TYPE_IPV4), tab);
+
+    // IPv6
+    tab = g_array_sized_new(FALSE, FALSE, sizeof(TpSocketAccessControl), 1);
+    ac = TP_SOCKET_ACCESS_CONTROL_LOCALHOST;
+    g_array_append_val(tab, ac);
+
+    if (supportMonitoring) {
+        ac = TP_SOCKET_ACCESS_CONTROL_PORT;
+        g_array_append_val(tab, ac);
+    }
+
+    g_hash_table_insert(ret, GUINT_TO_POINTER(TP_SOCKET_ADDRESS_TYPE_IPV6), tab);
+
+    // Named UNIX
+    tab = g_array_sized_new(FALSE, FALSE, sizeof(TpSocketAccessControl), 1);
+    ac = TP_SOCKET_ACCESS_CONTROL_LOCALHOST;
+    g_array_append_val(tab, ac);
+
+    if (supportMonitoring) {
+        ac = TP_SOCKET_ACCESS_CONTROL_CREDENTIALS;
+        g_array_append_val(tab, ac);
+    }
+
+    g_hash_table_insert(ret, GUINT_TO_POINTER(TP_SOCKET_ADDRESS_TYPE_UNIX), tab);
+
+    // Abstract UNIX
+    tab = g_array_sized_new(FALSE, FALSE, sizeof(TpSocketAccessControl), 1);
+    ac = TP_SOCKET_ACCESS_CONTROL_LOCALHOST;
+    g_array_append_val(tab, ac);
+
+    if (supportMonitoring) {
+        ac = TP_SOCKET_ACCESS_CONTROL_CREDENTIALS;
+        g_array_append_val(tab, ac);
+    }
+
+    g_hash_table_insert(ret, GUINT_TO_POINTER(TP_SOCKET_ADDRESS_TYPE_ABSTRACT_UNIX), tab);
 
     return ret;
 }
@@ -59,11 +258,16 @@ public:
         : Test(parent), mChanService(0)
     { }
 
+protected Q_SLOTS:
+    void onTubeRequested(const Tp::AccountPtr &, const Tp::OutgoingStreamTubeChannelPtr &,
+            const QDateTime &, const Tp::ChannelRequestHints &);
+
 private Q_SLOTS:
     void initTestCase();
     void init();
 
     void testRegistration();
+    void testBasicTcpExport();
 
     void cleanup();
     void cleanupTestCase();
@@ -71,26 +275,40 @@ private Q_SLOTS:
 private:
     QMap<QString, ClientHandlerInterface *> ourHandlers();
 
-    void createTubeChannel(bool requested, TpSocketAddressType addressType,
-            TpSocketAccessControl accessControl, bool withContact);
+    QPair<QString, QVariantMap> createTubeChannel(bool requested, HandleType type,
+            bool supportMonitoring);
 
+    AccountManagerPtr mAM;
+    AccountPtr mAcc;
     TestConnHelper *mConn;
     TpTestsStreamTubeChannel *mChanService;
-    StreamTubeChannelPtr mChan;
+
+    OutgoingStreamTubeChannelPtr mRequestedTube;
+    QDateTime mRequestTime;
+    ChannelRequestHints mRequestHints;
 };
 
-// TODO: turn into creating one (of possibly many) channels, which support everything, or everything
-// except Port and Creds ACs
-void TestStreamTubeHandlers::createTubeChannel(bool requested,
-        TpSocketAddressType addressType,
-        TpSocketAccessControl accessControl,
-        bool withContact)
+// TODO: turn into creating one (of possibly many) channels
+QPair<QString, QVariantMap> TestStreamTubeHandlers::createTubeChannel(bool requested,
+        HandleType handleType,
+        bool supportMonitoring)
 {
     mLoop->processEvents();
     tp_clear_object(&mChanService);
 
     /* Create service-side tube channel object */
-    QString chanPath = QString(QLatin1String("%1/Channel")).arg(mConn->objectPath());
+    QString chanPath = QString(QLatin1String("%1/Channel%2%3%4"))
+        .arg(mConn->objectPath())
+        .arg(requested)
+        .arg(static_cast<uint>(handleType))
+        .arg(supportMonitoring);
+    QVariantMap chanProps;
+
+    chanProps.insert(TP_QT4_IFACE_CHANNEL + QString::fromLatin1(".ChannelType"),
+            TP_QT4_IFACE_CHANNEL_TYPE_STREAM_TUBE);
+    chanProps.insert(TP_QT4_IFACE_CHANNEL + QString::fromLatin1(".Requested"), requested);
+    chanProps.insert(TP_QT4_IFACE_CHANNEL + QString::fromLatin1(".TargetHandleType"),
+            static_cast<uint>(handleType));
 
     TpHandleRepoIface *contactRepo = tp_base_connection_get_handles(
             TP_BASE_CONNECTION(mConn->service()), TP_HANDLE_TYPE_CONTACT);
@@ -98,17 +316,23 @@ void TestStreamTubeHandlers::createTubeChannel(bool requested,
             TP_BASE_CONNECTION(mConn->service()), TP_HANDLE_TYPE_ROOM);
     TpHandle handle;
     GType type;
-    if (withContact) {
+    if (handleType == HandleTypeContact) {
         handle = tp_handle_ensure(contactRepo, "bob", NULL, NULL);
         type = TP_TESTS_TYPE_CONTACT_STREAM_TUBE_CHANNEL;
+        chanProps.insert(TP_QT4_IFACE_CHANNEL + QString::fromLatin1(".TargetID"),
+                QString::fromLatin1("bob"));
     } else {
         handle = tp_handle_ensure(roomRepo, "#test", NULL, NULL);
         type = TP_TESTS_TYPE_ROOM_STREAM_TUBE_CHANNEL;
+        chanProps.insert(TP_QT4_IFACE_CHANNEL + QString::fromLatin1(".TargetID"),
+                QString::fromLatin1("#test"));
     }
+
+    chanProps.insert(TP_QT4_IFACE_CHANNEL + QString::fromLatin1(".TargetHandle"), handle);
 
     TpHandle alfHandle = tp_handle_ensure(contactRepo, "alf", NULL, NULL);
 
-    GHashTable *sockets = createSupportedSocketTypesHash(addressType, accessControl);
+    GHashTable *sockets = createSupportedSocketTypesHash(supportMonitoring);
 
     mChanService = TP_TESTS_STREAM_TUBE_CHANNEL(g_object_new(
             type,
@@ -120,17 +344,12 @@ void TestStreamTubeHandlers::createTubeChannel(bool requested,
             "initiator-handle", alfHandle,
             NULL));
 
-    /* Create client-side tube channel object */
-    GHashTable *props;
-    g_object_get(mChanService, "channel-properties", &props, NULL);
-
-    g_hash_table_unref(props);
-    g_hash_table_unref(sockets);
-
-    if (withContact)
+    if (handleType == HandleTypeContact)
         tp_handle_unref(contactRepo, handle);
     else
         tp_handle_unref(roomRepo, handle);
+
+    return qMakePair(chanPath, chanProps);
 }
 
 QMap<QString, ClientHandlerInterface *> TestStreamTubeHandlers::ourHandlers()
@@ -169,6 +388,36 @@ QMap<QString, ClientHandlerInterface *> TestStreamTubeHandlers::ourHandlers()
     return handlers;
 }
 
+void TestStreamTubeHandlers::onTubeRequested(
+        const Tp::AccountPtr &acc,
+        const Tp::OutgoingStreamTubeChannelPtr &tube,
+        const QDateTime &userActionTime,
+        const Tp::ChannelRequestHints &hints)
+{
+    qDebug() << "tube" << tube->objectPath() << "requested on account" << acc->objectPath();
+
+    // We don't use a shared factory here so the proxies will be different, but the object path
+    // should be the same
+    if (acc->objectPath() != mAcc->objectPath()) {
+        qWarning() << "account" << acc->objectPath() << "is not the expected" << mAcc->objectPath();
+        mLoop->exit(1);
+    }
+
+    // We always set the user action time in the past, so if that's carried over correctly, it won't
+    // be any more recent than the current time
+
+    if (mRequestTime >= QDateTime::currentDateTime()) {
+        qWarning() << "user action time later than expected";
+        mLoop->exit(2);
+    }
+
+    mRequestedTube = tube;
+    mRequestTime = userActionTime;
+    mRequestHints = hints;
+
+    mLoop->exit(0);
+}
+
 void TestStreamTubeHandlers::initTestCase()
 {
     initTestCaseImpl();
@@ -177,6 +426,24 @@ void TestStreamTubeHandlers::initTestCase()
     g_set_prgname("stream-tube-handlers");
     tp_debug_set_flags("all");
     dbus_g_bus_get(DBUS_BUS_STARTER, 0);
+
+    mAM = AccountManager::create();
+    QVERIFY(connect(mAM->becomeReady(),
+                    SIGNAL(finished(Tp::PendingOperation *)),
+                    SLOT(expectSuccessfulCall(Tp::PendingOperation *))));
+    QCOMPARE(mLoop->exec(), 0);
+    QCOMPARE(mAM->isReady(), true);
+
+    QVariantMap parameters;
+    parameters[QLatin1String("account")] = QLatin1String("foobar");
+    PendingAccount *pacc = mAM->createAccount(QLatin1String("foo"),
+            QLatin1String("bar"), QLatin1String("foobar"), parameters);
+    QVERIFY(connect(pacc,
+                    SIGNAL(finished(Tp::PendingOperation *)),
+                    SLOT(expectSuccessfulCall(Tp::PendingOperation *))));
+    QCOMPARE(mLoop->exec(), 0);
+    QVERIFY(pacc->account());
+    mAcc= pacc->account();
 
     mConn = new TestConnHelper(this,
             TP_TESTS_TYPE_SIMPLE_CONNECTION,
@@ -262,14 +529,82 @@ void TestStreamTubeHandlers::testRegistration()
     QVERIFY(handlers.contains(collaborationTool->clientName()));
 }
 
+void TestStreamTubeHandlers::testBasicTcpExport()
+{
+    StreamTubeServerPtr server =
+        StreamTubeServer::create(QStringList() << QLatin1String("ftp"), QStringList(),
+                QLatin1String("vsftpd"));
+    server->exportTcpSocket(QHostAddress::LocalHost, 22);
+    QVERIFY(server->isRegistered());
+
+    QMap<QString, ClientHandlerInterface *> handlers = ourHandlers();
+
+    QVERIFY(!handlers.isEmpty());
+    ClientHandlerInterface *handler = handlers.value(server->clientName());
+    QVERIFY(handler != 0);
+
+    ChannelClassList filter;
+    QVERIFY(waitForProperty(handler->requestPropertyHandlerChannelFilter(), &filter));
+
+    QCOMPARE(filter.size(), 1);
+    QVERIFY(ChannelClassSpec(filter.first())
+            == ChannelClassSpec::outgoingStreamTube(QLatin1String("ftp")));
+
+    QPair<QString, QVariantMap> chan = createTubeChannel(true, HandleTypeContact, false);
+
+    QVERIFY(connect(server.data(),
+                SIGNAL(tubeRequested(Tp::AccountPtr,Tp::OutgoingStreamTubeChannelPtr,QDateTime,Tp::ChannelRequestHints)),
+                SLOT(onTubeRequested(Tp::AccountPtr,Tp::OutgoingStreamTubeChannelPtr,QDateTime,Tp::ChannelRequestHints))));
+
+    QDateTime userActionTime = QDateTime::currentDateTime().addDays(-1);
+    userActionTime = userActionTime.addMSecs(-userActionTime.time().msec());
+    qDebug() << userActionTime;
+
+    QVariantMap hints;
+    hints.insert(QLatin1String("tp-qt4-test-request-hint-herring-color-rgba"), uint(0xff000000));
+
+    QObject *request = new QObject(this);
+    QString requestPath = QLatin1String("/org/freedesktop/Telepathy/ChannelRequest/Request1");
+
+    QDBusConnection bus = server->registrar()->dbusConnection();
+    new ChannelRequestAdaptor(QDBusObjectPath(mAcc->objectPath()),
+            userActionTime.toTime_t(),
+            QString(),
+            QualifiedPropertyValueMapList(),
+            QStringList(),
+            false,
+            false,
+            hints,
+            request);
+    QVERIFY(bus.registerService(TP_QT4_CHANNEL_DISPATCHER_BUS_NAME));
+    QVERIFY(bus.registerObject(requestPath, request));
+
+    ChannelDetails details = { QDBusObjectPath(chan.first), chan.second };
+    handler->HandleChannels(
+            QDBusObjectPath(mAcc->objectPath()),
+            QDBusObjectPath(mConn->objectPath()),
+            ChannelDetailsList() << details,
+            ObjectPathList() << QDBusObjectPath(requestPath),
+            userActionTime.toTime_t(),
+            QVariantMap());
+
+    QCOMPARE(mLoop->exec(), 0);
+
+    QCOMPARE(mRequestedTube->objectPath(), chan.first);
+    QCOMPARE(mRequestTime, userActionTime);
+    QCOMPARE(mRequestHints.allHints(), hints);
+}
+
 void TestStreamTubeHandlers::cleanup()
 {
     cleanupImpl();
 
-    if (mChan && mChan->isValid()) {
+    mRequestHints = ChannelRequestHints();
+
+    if (mRequestedTube && mRequestedTube->isValid()) {
         qDebug() << "waiting for the channel to become invalidated";
 
-        QVERIFY(connect(mChan.data(),
+        QVERIFY(connect(mRequestedTube.data(),
                 SIGNAL(invalidated(Tp::DBusProxy*,QString,QString)),
                 mLoop,
                 SLOT(quit())));
@@ -277,7 +612,7 @@ void TestStreamTubeHandlers::cleanup()
         QCOMPARE(mLoop->exec(), 0);
     }
 
-    mChan.reset();
+    mRequestedTube.reset();
 
     if (mChanService != 0) {
         g_object_unref(mChanService);
@@ -289,6 +624,9 @@ void TestStreamTubeHandlers::cleanup()
 
 void TestStreamTubeHandlers::cleanupTestCase()
 {
+    mAM.reset();
+    mAcc.reset();
+
     QCOMPARE(mConn->disconnect(), true);
     delete mConn;
 
