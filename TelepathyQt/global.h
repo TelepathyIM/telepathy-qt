3ca21aa285473373f673bb0bee596be4de5dc429
Andre Moreira Magalhaes (andrunko) <andre.magalhaes@collabora.co.uk>
2011-11-23 20:46:33 -0200

    global: Change doxygen group for macros.

--- TelepathyQt/global.h
+++ TelepathyQt/global.h
@@ -45,7 +45,7 @@
 
 /**
  * @def TP_QT_DEPRECATED
- * @ingroup TELEPATHY_QT_MACROS
+ * @ingroup macros
  *
  * The TP_QT_DEPRECATED macro can be used to trigger compile-time
  * warnings with newer compilers when deprecated functions are used.
