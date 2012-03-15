2a99ef01b3fc99602894081c655b4783a89f500e
Andre Moreira Magalhaes (andrunko) <andre.magalhaes@collabora.co.uk>
2012-03-15 01:36:31 -0300

    BaseConnection: Add default param to registerObject.

--- TelepathyQt/base-connection.h
+++ TelepathyQt/base-connection.h
@@ -66,7 +66,7 @@ public:
     QVariantMap parameters() const;
 
     virtual QString uniqueName() const;
-    bool registerObject(DBusError *error);
+    bool registerObject(DBusError *error = NULL);
 
 Q_SIGNALS:
     void disconnected();
