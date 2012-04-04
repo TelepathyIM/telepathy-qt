6c258da57b40dab608c31b6abdff93d2792bf9c0
Andre Moreira Magalhaes (andrunko) <andre.magalhaes@collabora.co.uk>
2012-04-04 11:16:15 -0300

    DBusObject: Add utility class holding a QDBusConnection.

---
+++ TelepathyQt/dbus-object.cpp
@@ -0,0 +1,56 @@
+/**
+ * This file is part of TelepathyQt
+ *
+ * @copyright Copyright (C) 2012 Collabora Ltd. <http://www.collabora.co.uk/>
+ * @copyright Copyright (C) 2012 Nokia Corporation
+ * @license LGPL 2.1
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <TelepathyQt/DBusObject>
+
+#include <QDBusConnection>
+
+namespace Tp
+{
+
+struct TP_QT_NO_EXPORT DBusObject::Private
+{
+    Private(const QDBusConnection &dbusConnection)
+        : dbusConnection(dbusConnection)
+    {
+    }
+
+    QDBusConnection dbusConnection;
+};
+
+DBusObject::DBusObject(const QDBusConnection &dbusConnection, QObject *parent)
+    : QObject(parent),
+      mPriv(new Private(dbusConnection))
+{
+}
+
+DBusObject::~DBusObject()
+{
+    delete mPriv;
+}
+
+QDBusConnection DBusObject::dbusConnection() const
+{
+    return mPriv->dbusConnection;
+}
+
+}
