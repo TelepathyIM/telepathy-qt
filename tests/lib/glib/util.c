5cf2d1a911b2e57a888574b7e2b7f7a546435051
George Kiagiadakis <george.kiagiadakis@collabora.com>
2012-05-23 15:20:03 +0300

    tests/lib/glib/util.c: Remove tp_tests_connection_run_request_contact_handle
    This method uses deprecated API and it's not used anywhere, so it's not
    worth porting.

--- tests/lib/glib/util.c
+++ tests/lib/glib/util.c
@@ -84,50 +84,6 @@ tp_tests_proxy_run_until_dbus_queue_processed (gpointer proxy)
   g_main_loop_unref (loop);
 }
 
-typedef struct {
-    GMainLoop *loop;
-    TpHandle handle;
-} HandleRequestResult;
-
-static void
-handles_requested_cb (TpConnection *connection G_GNUC_UNUSED,
-    TpHandleType handle_type G_GNUC_UNUSED,
-    guint n_handles,
-    const TpHandle *handles,
-    const gchar * const *ids G_GNUC_UNUSED,
-    const GError *error,
-    gpointer user_data,
-    GObject *weak_object G_GNUC_UNUSED)
-{
-  HandleRequestResult *result = user_data;
-
-  g_assert_no_error ((GError *) error);
-  g_assert_cmpuint (n_handles, ==, 1);
-  result->handle = handles[0];
-}
-
-static void
-handle_request_result_finish (gpointer r)
-{
-  HandleRequestResult *result = r;
-
-  g_main_loop_quit (result->loop);
-}
-
-TpHandle
-tp_tests_connection_run_request_contact_handle (TpConnection *connection,
-    const gchar *id)
-{
-  HandleRequestResult result = { g_main_loop_new (NULL, FALSE), 0 };
-  const gchar * const ids[] = { id, NULL };
-
-  tp_connection_request_handles (connection, -1, TP_HANDLE_TYPE_CONTACT, ids,
-      handles_requested_cb, &result, handle_request_result_finish, NULL);
-  g_main_loop_run (result.loop);
-  g_main_loop_unref (result.loop);
-  return result.handle;
-}
-
 void
 _test_assert_empty_strv (const char *file,
     int line,
