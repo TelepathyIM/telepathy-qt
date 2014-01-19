9d349d4d0a56bbd5fe0799193448a53c6247d14a
David Edmundson <kde@davidedmundson.co.uk>
2014-01-19 23:12:52 +0100

    Fix ChannelDispatcher.Messages test

--- tests/dbus/contact-messenger.cpp
+++ tests/dbus/contact-messenger.cpp
@@ -44,9 +44,9 @@ class TestContactMessenger;
 class CDMessagesAdaptor : public QDBusAbstractAdaptor
 {
     Q_OBJECT
-    Q_CLASSINFO("D-Bus Interface", "org.freedesktop.Telepathy.ChannelDispatcher.Interface.Messages.DRAFT")
+    Q_CLASSINFO("D-Bus Interface", "org.freedesktop.Telepathy.ChannelDispatcher.Interface.Messages1")
     Q_CLASSINFO("D-Bus Introspection", ""
-"  <interface name=\"org.freedesktop.Telepathy.ChannelDispatcher.Interface.Messages.DRAFT\" >\n"
+"  <interface name=\"org.freedesktop.Telepathy.ChannelDispatcher.Interface.Messages1\" >\n"
 "    <method name=\"SendMessage\" >\n"
 "      <arg name=\"Account\" type=\"o\" direction=\"in\" />\n"
 "      <arg name=\"TargetID\" type=\"s\" direction=\"in\" />\n"
